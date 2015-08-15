bd76eb6a9cf232ec9281de807ed374c98406ad20
Yusuke Goto <u.suke.goto@gmail.com>
2015-08-15 23:01:24 +0900

    static home and help

---
+++ web/router.ex
@@ -0,0 +1,27 @@
+defmodule SampleApp.Router do
+  use SampleApp.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", SampleApp do
+    pipe_through :browser # Use the default browser stack
+
+    get "/static_page/home", StaticPageController, :home
+    get "/static_page/help", StaticPageController, :help
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", SampleApp do
+  #   pipe_through :api
+  # end
+end
