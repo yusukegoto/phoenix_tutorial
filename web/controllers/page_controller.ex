d8e1ad3af95241411c562966a4459950e178abfd
Yusuke Goto <u.suke.goto@gmail.com>
2015-08-16 19:39:12 +0900

    title helperize

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule SampleApp.PageController do
   use SampleApp.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", title: nil
   end
 end
--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,13 @@
 defmodule SampleApp.LayoutView do
   use SampleApp.Web, :view
+
+  def full_title(title) do
+    result = "Phoenix!"
+
+    if title && title != "" do
+      result = result <> " #{title}"
+    end
+
+    result
+  end
 end
