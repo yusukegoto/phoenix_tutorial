0b7c522bf2c5ac04bfe4644968f9847c1a97ee3f
Yusuke Goto <u.suke.goto@gmail.com>
2015-08-16 12:05:04 +0900

    title

--- web/controllers/static_page_controller.ex
+++ web/controllers/static_page_controller.ex
@@ -2,14 +2,14 @@ defmodule SampleApp.StaticPageController do
   use SampleApp.Web, :controller
 
   def home(conn, _params) do
-    render conn, "home.html"
+    render conn, "home.html", title: "Home"
   end
 
   def help(conn, _params) do
-    render conn, "help.html"
+    render conn, "help.html", title: "Help"
   end
 
   def about(conn, _params) do
-    render conn, "about.html"
+    render conn, "about.html", title: "About Us"
   end
 end
