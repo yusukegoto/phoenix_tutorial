82e2aca7e9c75497546ff9252d468b845b1b3a78
Yusuke Goto <u.suke.goto@gmail.com>
2015-08-23 00:15:25 +0900

    add test

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,16 +3,21 @@ defmodule SampleApp.UserTest do
 
   alias SampleApp.User
 
-  @valid_attrs %{name: "test", email: "test@test.com"}
-  @invalid_attrs %{}
+  @valid_attrs   %{name: "test", email: "test@test.com"}
+  @invalid_attrs %{emal: "error"}
 
   test "changeset with valid attributes" do
     changeset = User.changeset(%User{}, @valid_attrs)
     assert changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
+  test "changeset with valid attributes" do
     changeset = User.changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    assert !changeset.valid?
   end
 end
